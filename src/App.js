80ceda2201fafe748c003e045bad6d6663cd6df3
clintjeez <clintjeez@gmail.com>
2021-01-10 15:14:10 +0100

    add hero component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/header/header";
+import Hero from "./components/hero/hero";
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header />
+        <Hero />
       </div>
-      hello
     </div>
   );
 }
